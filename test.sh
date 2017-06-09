6927bb9351e664d2d4b9588cb30f23bcf1a62dd5
Etienne Caldichoury <etienne@MacBook-Pro-de-Etienne.local>
2017-06-09 23:33:38 +0200

    Add some lines to script to check duplicated

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 myDate=`date`
 echo $myDate
 curl -H "Content-Type: application/json" -H "X-Auth-User: 1234" -X POST -d '}' http://localhost:8080/api/gifs/favorite 
+curl -H "Content-Type: application/json" -H "X-Auth-User: 1234" -X POST -d '{"date":"tata"}' http://localhost:8080/api/gifs/favorite 
+curl -H "Content-Type: application/json" -H "X-Auth-User: 1234" -X POST -d '{"date":"tata"}' http://localhost:8080/api/gifs/favorite 
+curl -H "Content-Type: application/json" -H "X-Auth-User: 1234" -X POST -d '{"date":"tata"}' http://localhost:8080/api/gifs/favorite 
+
 curl -H "Content-Type: application/json" -H "X-Auth-User: 1234" -X POST -d '{"date":"'"$myDate"'"}' http://localhost:8080/api/gifs/favorite 
 curl -H "Content-Type: application/json" -H "X-Auth-User: 1233" -X POST -d '{"date":"'"$myDate"'"}' http://localhost:8080/api/emojis/favorite 
 curl -H "Content-Type: application/json" -H "X-Auth-User: 2234" -X POST -d '{"date":"'"$myDate"'"}' http://localhost:8080/api/gifs/favorite 
