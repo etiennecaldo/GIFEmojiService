9b578713eae75112146c6de9962b907dd09dedcc
Etienne Caldichoury <etienne@MacBook-Pro-de-Etienne.local>
2017-06-09 20:22:51 +0200

    [DEV] Interface with giphy api

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const app = express()
+var giphy = require('giphy-api')();
 
-app.get('/api/:type/search/:query', function (req, res) {
+app.get('/api/:type/search/', function (req, res) {
     try 
     {
         switch(req.params.type) {
             case 'emoji':
             case 'gif':
-                console.log(req.params.type);
+                req.query.limit=20
+                giphy.search(req.query, function (err, response) {
+                    gifs = response.data || []
+                    res.send(gifs.map(function(d) { return d["embed_url"]; }));
+                });
                 break;
             default:
                 throw "Unknown type: " + req.params.type;
         }
-        res.send(req.params.type)
+        //res.send(req.params.type)
     }
     catch(err)
     {
@@ -24,11 +29,19 @@ function sendError(res, status, err) {
     res.status(status).send({"error":err});
 }
 
+
+app.get('/', function (req, res) {
+    console.log(req.query);
+    
+    
+});
+/*
 app.get('*', function (req, res) {
     sendError(res, 404, "Cannot get " + req.path);
 })
-
+*/
 var port = process.env.port || 8080;
 app.listen(port, function () {
     console.log('Example app listening on port ' + port + ' !')
 })
+
